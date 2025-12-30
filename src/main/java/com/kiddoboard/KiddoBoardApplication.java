53e1077af23ba1df9b2e68a3b0f5a9689689a6df
Mateusz Michalski <mmichalskey@gmail.com>
2025-12-30 13:54:27 +0100

    feat: project init with run app from docker config

---
+++ src/main/java/com/kiddoboard/KiddoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kiddoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiddoBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiddoBoardApplication.class, args);
+    }
+
+}
